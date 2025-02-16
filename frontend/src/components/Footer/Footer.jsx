bf2f242ca804b54355ca818ec2f7e94c010b025e
Parthav <parthavsabrwal@gmail.com>
2025-02-17 00:36:01 +0530

    Latest

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./footer.css";
+import "./Footer.css";
 import { FaEnvelope, FaPhone } from "react-icons/fa";
 import mylogo from '../../assets/logo.png';
 import MobileFooter from "./MobileFooter";
